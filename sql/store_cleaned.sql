8fec856d3bdab92df86e78fcd4069d7ed04b5ce1
MaksymYakushev <yakushev0022@gmail.com>
2026-04-20 19:41:01 +0200

    Created store_cleaned.sql

---
+++ sql/store_cleaned.sql
@@ -0,0 +1,10 @@
+/************************************************************
+ * File:    store_cleaned.sql
+ * Purpose: Cleaned dataset for analysis and visualization
+ * Author:  Maksym Yakushev
+ * Date:    2026-04-20
+ ************************************************************/
+ 
+SELECT 
+*
+FROM store;
