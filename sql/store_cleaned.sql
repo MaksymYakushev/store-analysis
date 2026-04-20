98b33ee4e9342ac829151f9105ed829d6c2c8f24
MaksymYakushev <yakushev0022@gmail.com>
2026-04-20 23:26:04 +0200

    update cleaning logic and add tableau workspace

--- sql/store_cleaned.sql
+++ sql/store_cleaned.sql
@@ -4,7 +4,30 @@
  * Author:  Maksym Yakushev
  * Date:    2026-04-20
  ************************************************************/
- 
+
+
+-- ============================================================
+-- 1. DATA PREVIEW
+-- ============================================================
+
 SELECT 
 *
 FROM store;
+
+
+-- ============================================================
+-- 2. CLEANED DATASET – VIEW
+-- ============================================================
+
+CREATE VIEW store_cleaned AS
+SELECT *
+FROM (
+    SELECT
+        *
+        , ROW_NUMBER() OVER(PARTITION BY order_id, product_id ORDER BY order_id) AS rnk
+    FROM store
+    WHERE 
+		LENGTH(postal_code::TEXT) = 5
+) AS t
+WHERE rnk = 1;
+
